9e456fbf1e4228f03da3112929cff8c597593619
elyseleong <elyse.xuan@gmail.com>
2023-11-27 17:02:47 +0800

    Made comments

--- Generate_PCSP.py
+++ Generate_PCSP.py
@@ -1,6 +1,10 @@
 import math
-from distributions_updated import yardage, fit_and_estimate_probabilities
+from distributions import yardage, fit_and_estimate_probabilities
 from data_processing import type as get_data_for_team
+
+# These can be adjusted to accommodate various requirements.
+TEAM = "KC"
+OPP_TEAM = "NYJ"
 
 # Convenience constants for line breaks and indentation.
 BR = "\n"
@@ -9,19 +13,16 @@ BR_2TAB = "\n\t\t"
 BR_3TAB = "\n\t\t\t"
 BR_4TAB = "\n\t\t\t\t"
 
-# These can be adjusted to accommodate various requirements.
-TEAM = "KC"
-OPP_TEAM = "CLE"
-
 ZONE = 4
 DOWN = ['1st', '2nd', '3rd', '4th'] 
 PLAYS = ['run', 'pass','pass_incomp', 'punt', 'field_goal', 'turnover']
 
 # Use to generate probabilities for a play advancing YARDAGE_INCREMENT
-YARDAGE_INCREASE = list(range(0, MAX_YARDAGE + 1, YARDAGE_INCREMENT))
 YARDAGE_INCREMENT = 4
 MAX_YARDAGE = 20
+YARDAGE_INCREASE = list(range(0, MAX_YARDAGE + 1, YARDAGE_INCREMENT))
 
+# Generate model logic strings
 def generate_model_string(ZONE, DOWN, PLAYS, YARDAGE_INCREASE):
     model_string = "GuardedDown = [!GAME_OVER]Go->ExecuteDown;\n\n" + "// Guarded process that only runs the next down if the attacking team has not completed all 4.\n" + "NextPlay = GuardedDown[][GAME_OVER] Skip;\n\n" + "// Function to simulate a single down. Only executes if the attacking team has not used all 4 downs.\n"
 
@@ -58,16 +59,16 @@ def generate_model_string(ZONE, DOWN, PLAYS, YARDAGE_INCREASE):
     model_string += f"{BR}}};"
     return model_string
 
-# generate pcsp file
+# Generate pcsp file
 def generate_pcsp():
     SETUP = 'script_model_setup.txt'
     HELPER_FUNC = 'script_helper_function.txt'
     ASSERTIONS = 'script_assertions.txt'
 
     model_string = generate_model_string(ZONE, DOWN, PLAYS, YARDAGE_INCREASE)
-    play_probabilities_df = get_data_for_team(TEAM)
+    play_probabilities_df = get_data_for_team(TEAM) # from data_processing.py
     data = yardage(TEAM)
-    yard_probabilities_dict = fit_and_estimate_probabilities(data, TEAM, OPP_TEAM)
+    yard_probabilities_dict = fit_and_estimate_probabilities(data, TEAM, OPP_TEAM) # from distributions.py
     
     file_name = '%s_%s.pcsp' % (TEAM, OPP_TEAM)
     file_path = 'output/' + file_name
@@ -75,14 +76,15 @@ def generate_pcsp():
     setup_string = []
     with open(SETUP) as f:
         setup_string = f.readlines()
-        
+    
+    # Probabilities for play selection
     play_probabilities_string = "\n"
     for index, row in play_probabilities_df.iterrows():
         play_probabilities_string += '#define _%s %d;\n' % (row["Value"], row["Count"])
 
+    # Probabilities for yardage gained
     yard_probabilities_string = "\n"
     for p_name, p in yard_probabilities_dict.items():
-        #TODO: make it dynamic
         p = int(math.floor(p * 1000))
         yard_probabilities_string += '#define _%s %d;\n' % (p_name, p)
     yard_probabilities_string += "\n//---- End Probabilities Setup ----//\n\n"
